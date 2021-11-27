408afdf8f4b97f57cf9b15eccd7c47327c20c806
cherrysrc <50867655+cherrysrc@users.noreply.github.com>
2021-11-27 12:23:07 +0100

    Restructured tests

--- tests/gl_test.rs
+++ tests/gl_test.rs
@@ -1,25 +1,10 @@
-//! Use "cargo test -- --nocapture" for details on which test failed exactly.
-//! Testing these OpenGL features is more difficult since they need an OpenGL context to work in.
+//! Use "cargo test -- --test-threads=1" to test this crate.
 //! GLFW windows aren't too happy about being created in parallel (as cargo runs it's tests).
-//! At least that's what I suspect the issue to be. That's the reason behind the odd test structure.
 
-mod ssbo_test {
-    use open_rl::{ComputeShader, GpuSsbo, ShaderSource, SSBO};
-
-    pub const COMPUTE_SHADER: &str = "
-        #version 430
-        layout(local_size_x = 1, local_size_y = 1) in;
-
-        layout(std430, binding = 0) buffer Resolution {
-            uint x;
-            uint y;
-        } resolution;
-
-        void main() {
-            resolution.x = 400;
-            resolution.y = 400;
-        }
-    ";
+#[cfg(test)]
+mod tests {
+    use glfw::Context;
+    use open_rl::{ComputeShader, GpuSsbo, SSBO, ShaderSource};
 
     pub struct Resolution {
         x: u32,
@@ -28,123 +13,8 @@ mod ssbo_test {
 
     impl GpuSsbo for Resolution {}
 
-    pub fn setup(
-    ) -> Result<(ComputeShader, Resolution, (u32, u32), SSBO), Box<dyn std::error::Error>> {
-        let shader = ComputeShader::create(ShaderSource::String(COMPUTE_SHADER))?;
-
-        let resolution_struct = Resolution { x: 200, y: 200 };
-        let resolution_tuple = (100 as u32, 100 as u32); //TODO use
-
-        let ssbo = SSBO::create_from(0, &resolution_struct, gl::STATIC_DRAW);
-
-        Ok((shader, resolution_struct, resolution_tuple, ssbo))
-    }
-
-    pub fn verify_setup(
-        setup: &(ComputeShader, Resolution, (u32, u32), SSBO),
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let mut resolution_on_gpu = Resolution { x: 1, y: 1 };
-        resolution_on_gpu.load_from(&setup.3, 0);
-
-        assert_eq!(resolution_on_gpu.x, setup.1.x);
-        assert_eq!(resolution_on_gpu.y, setup.1.y);
-
-        setup.2.save_to(&setup.3, 0);
-        resolution_on_gpu.load_from(&setup.3, 0);
-
-        assert_eq!(resolution_on_gpu.x, setup.2 .0);
-        assert_eq!(resolution_on_gpu.y, setup.2 .1);
-
-        Ok(())
-    }
-
-    pub fn step(
-        setup: &(ComputeShader, Resolution, (u32, u32), SSBO),
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        setup.0.dispatch(1, 1, 1, gl::SHADER_STORAGE_BARRIER_BIT);
-        Ok(())
-    }
-
-    pub fn verify_step(
-        setup: &(ComputeShader, Resolution, (u32, u32), SSBO),
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let mut resolution_on_gpu = Resolution { x: 1, y: 1 };
-        resolution_on_gpu.load_from(&setup.3, 0);
-
-        assert_eq!(resolution_on_gpu.x, 400);
-        assert_eq!(resolution_on_gpu.y, 400);
-
-        let mut tup = (0 as u32, 0 as u32);
-        tup.load_from(&setup.3, 0);
-
-        assert_eq!(tup.0, 400);
-        assert_eq!(tup.1, 400);
-
-        Ok(())
-    }
-}
-
-mod compute_shader_test {
-    use open_rl::{ComputeShader, GpuSsbo, ShaderSource, SSBO};
-
-    pub const COMPUTE_SHADER: &str = "        
-        #version 430
-        layout(local_size_x = 1, local_size_y = 1) in;
-
-        layout(std430, binding = 1) buffer Content {
-            uint[] content;
-        } arr;
-
-        void main() {
-            arr.content[gl_GlobalInvocationID.x] = 123;
-        }
-    ";
-
-    pub fn setup() -> Result<(ComputeShader, SSBO, Vec<u32>), Box<dyn std::error::Error>> {
-        let shader = ComputeShader::create(ShaderSource::String(COMPUTE_SHADER))?;
-        let vec = vec![0 as u32; 10];
-
-        let ssbo = SSBO::create_from(1, &vec, gl::STATIC_DRAW);
-
-        Ok((shader, ssbo, vec))
-    }
-
-    pub fn verify_setup(
-        setup: &(ComputeShader, SSBO, Vec<u32>),
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let mut vec_on_gpu = vec![0 as u32; 10];
-        vec_on_gpu.load_from(&setup.1, 0);
-
-        assert_eq!(vec_on_gpu, setup.2);
-
-        Ok(())
-    }
-
-    pub fn step(setup: &(ComputeShader, SSBO, Vec<u32>)) -> Result<(), Box<dyn std::error::Error>> {
-        setup.0.dispatch(10, 1, 1, gl::SHADER_STORAGE_BARRIER_BIT);
-        Ok(())
-    }
-
-    pub fn verify_step(
-        setup: &(ComputeShader, SSBO, Vec<u32>),
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let mut vec_on_gpu = vec![0 as u32; 10];
-        vec_on_gpu.load_from(&setup.1, 0);
-
-        assert_eq!(vec_on_gpu, vec![123 as u32; 10]);
-
-        Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use glfw::Context;
-
-    use crate::{compute_shader_test, ssbo_test};
-
     #[test]
-    fn gl_test() -> Result<(), Box<dyn std::error::Error>> {
+    fn ssbo_test() -> Result<(), Box<dyn std::error::Error>> {
         let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS)?;
         let (mut window, _) = glfw
             .create_window(100, 100, "Test", glfw::WindowMode::Windowed)
@@ -156,13 +26,45 @@ mod tests {
 
         glfw.set_swap_interval(glfw::SwapInterval::Sync(1));
 
+        //---------
         //Test setup
-        let ssbo_test_setup = ssbo_test::setup()?;
-        let compute_shader_test_setup = compute_shader_test::setup()?;
+        //---------
+        pub const COMPUTE_SHADER: &str = "
+            #version 430
+            layout(local_size_x = 1, local_size_y = 1) in;
 
+            layout(std430, binding = 0) buffer Resolution {
+                uint x;
+                uint y;
+            } resolution;
+
+            void main() {
+                resolution.x = 400;
+                resolution.y = 400;
+            }
+        ";
+
+        let shader = ComputeShader::create(ShaderSource::String(COMPUTE_SHADER))?;
+
+        let resolution_struct = Resolution { x: 200, y: 200 };
+        let mut resolution_tuple = (100 as u32, 100 as u32); //TODO use
+
+        let ssbo = SSBO::create_from(0, &resolution_struct, gl::STATIC_DRAW);
+
+        //---------
         //Test setup verification
-        ssbo_test::verify_setup(&ssbo_test_setup)?;
-        compute_shader_test::verify_setup(&compute_shader_test_setup)?;
+        //---------
+        let mut resolution_on_gpu = Resolution {x:1,y:1};
+        resolution_on_gpu.load_from(&ssbo, 0);
+
+        assert_eq!(resolution_on_gpu.x, resolution_on_gpu.x);
+        assert_eq!(resolution_on_gpu.y, resolution_on_gpu.y);
+
+        resolution_tuple.save_to(&ssbo, 0);
+        resolution_on_gpu.load_from(&ssbo, 0);
+
+        assert_eq!(resolution_on_gpu.x, resolution_tuple.0);
+        assert_eq!(resolution_on_gpu.y, resolution_tuple.1);
 
         while !window.should_close() {
             unsafe {
@@ -171,14 +73,81 @@ mod tests {
             }
 
             //Test step
-            ssbo_test::step(&ssbo_test_setup)?;
-            compute_shader_test::step(&compute_shader_test_setup)?;
+            shader.dispatch(1, 1, 1, gl::SHADER_STORAGE_BARRIER_BIT);
 
             window.swap_buffers();
 
             //Test step verification
-            ssbo_test::verify_step(&ssbo_test_setup)?;
-            compute_shader_test::verify_step(&compute_shader_test_setup)?;
+            resolution_on_gpu.load_from(&ssbo, 0);
+
+            assert_eq!(resolution_on_gpu.x, 400);
+            assert_eq!(resolution_on_gpu.y, 400);
+
+            resolution_tuple.load_from(&ssbo, 0);
+            assert_eq!(resolution_tuple, (400,400));
+
+            window.set_should_close(true);
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn compute_shader_test() -> Result<(), Box<dyn std::error::Error>> {
+        let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS)?;
+        let (mut window, _) = glfw
+            .create_window(100, 100, "Test", glfw::WindowMode::Windowed)
+            .expect("");
+
+        window.make_current();
+
+        gl::load_with(|s| window.get_proc_address(s) as *const _);
+
+        glfw.set_swap_interval(glfw::SwapInterval::Sync(1));
+
+        //-----------
+        //Test setup
+        //-----------
+        pub const COMPUTE_SHADER: &str = "        
+            #version 430
+            layout(local_size_x = 1, local_size_y = 1) in;
+
+            layout(std430, binding = 1) buffer Content {
+                uint[] content;
+            } arr;
+
+            void main() {
+                arr.content[gl_GlobalInvocationID.x] = 123;
+            }
+        ";
+
+        let shader = ComputeShader::create(ShaderSource::String(COMPUTE_SHADER))?;
+        let vec = vec![0 as u32; 10];
+
+        let ssbo = SSBO::create_from(1, &vec, gl::STATIC_DRAW);
+
+        //-----------
+        //Test setup verification
+        //-----------
+        let mut vec_on_gpu = vec![0 as u32; 10];
+        vec_on_gpu.load_from(&ssbo, 0);
+
+        assert_eq!(vec_on_gpu, vec);
+
+        while !window.should_close() {
+            unsafe {
+                gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+                gl::Clear(gl::COLOR_BUFFER_BIT);
+            }
+
+            //Test step
+            shader.dispatch(10, 1, 1, gl::SHADER_STORAGE_BARRIER_BIT);
+
+            window.swap_buffers();
+
+            //Test step verification
+            vec_on_gpu.load_from(&ssbo, 0);
+            assert_eq!(vec_on_gpu, vec![123 as u32; 10]);
 
             window.set_should_close(true);
         }
