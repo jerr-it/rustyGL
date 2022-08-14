d1697df43420d51860c18aee648ce6d25786bd3a
cherrysrc <50867655+cherrysrc@users.noreply.github.com>
2022-08-14 10:45:26 +0200

    Adjusted customshape

--- src/shapes/custom.rs
+++ src/shapes/custom.rs
@@ -12,19 +12,23 @@ pub struct CustomShape {
     draw_mode: gl::types::GLenum,
 
     vao: VAO,
-    vbo: VBO,
+    _vbo: VBO,
 }
 
 impl Drawable for CustomShape {
     fn draw(&self) {
-        self.vao.draw(self.draw_mode, self.vertices.len() as i32, false);
+        self.vao
+            .draw(self.draw_mode, self.vertices.len() as i32, false);
     }
 }
 
 impl CustomShape {
     pub fn new(
-        vertices: Vec<Vertex>, draw_mode: gl::types::GLenum,
-        position_location: Option<u32>, color_location: Option<u32>, uv_location: Option<u32>
+        vertices: Vec<Vertex>,
+        draw_mode: gl::types::GLenum,
+        position_location: Option<u32>,
+        color_location: Option<u32>,
+        uv_location: Option<u32>,
     ) -> CustomShape {
         let vao = VAO::new();
         let vbo = VBO::new(Some(&vertices));
@@ -33,11 +37,11 @@ impl CustomShape {
             match position_location {
                 Some(location) => location,
                 None => 0,
-            }, 
-            3, 
-            gl::FLOAT, 
-            gl::FALSE, 
-            std::mem::size_of::<Vertex>() as i32, 
+            },
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            std::mem::size_of::<Vertex>() as i32,
             std::ptr::null(),
         );
 
@@ -45,11 +49,11 @@ impl CustomShape {
             match color_location {
                 Some(location) => location,
                 None => 1,
-            }, 
-            3, 
-            gl::FLOAT, 
-            gl::FALSE, 
-            std::mem::size_of::<Vertex>() as i32, 
+            },
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            std::mem::size_of::<Vertex>() as i32,
             unsafe { std::ptr::null::<Vector3<f32>>().add(1) as *const c_void },
         );
 
@@ -57,14 +61,19 @@ impl CustomShape {
             match uv_location {
                 Some(location) => location,
                 None => 2,
-            },  
-            2, 
-            gl::FLOAT, 
-            gl::FALSE, 
-            std::mem::size_of::<Vertex>() as i32, 
-            unsafe { std::ptr::null::<Vector3<f32>>().add(2) as *const c_void }
+            },
+            2,
+            gl::FLOAT,
+            gl::FALSE,
+            std::mem::size_of::<Vertex>() as i32,
+            unsafe { std::ptr::null::<Vector3<f32>>().add(2) as *const c_void },
         );
 
-        CustomShape { vertices, draw_mode, vao, vbo }
+        CustomShape {
+            vertices,
+            draw_mode,
+            vao,
+            _vbo: vbo,
+        }
     }
 }
